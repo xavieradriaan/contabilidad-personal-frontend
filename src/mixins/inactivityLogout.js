22942c1296a233877ef8af7fb002eb30bad30e4d
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2024-11-24 16:32:34 -0500

    funcionalTerminado implementacion de tiempo en pantalla inactividad 5 minutos

--- src/mixins/inactivityLogout.js
+++ src/mixins/inactivityLogout.js
@@ -2,7 +2,7 @@ export default {
   data() {
     return {
       inactivityTimeout: null,
-      logoutTime: 8 * 1000 // 8 segundos en milisegundos
+      logoutTime: 5 * 60 * 1000 // 8 segundos en milisegundos
     };
   },
   methods: {
