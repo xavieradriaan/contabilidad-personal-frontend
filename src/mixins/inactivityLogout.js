14094dbf3cd78a958674d4aaa0560834d42e1528
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2024-11-24 18:21:02 -0500

    seee

--- src/mixins/inactivityLogout.js
+++ src/mixins/inactivityLogout.js
@@ -2,37 +2,36 @@ export default {
   data() {
     return {
       inactivityTimeout: null,
-      logoutTime: 5 * 60 * 1000 // 8 segundos en milisegundos
+      logoutTime: 8 * 1000 // 8 segundos en milisegundos
     };
   },
   methods: {
     resetInactivityTimeout() {
-      if (localStorage.getItem('token')) {
-        clearTimeout(this.inactivityTimeout);
-        this.inactivityTimeout = setTimeout(this.logoutUser, this.logoutTime);
-        console.log('Inactivity timeout reset');
-      },
-      logoutUser() {
-        localStorage.removeItem('token');
-        localStorage.removeItem('username');
-        this.$router.push('/login');
-        this.$swal.fire({
-          icon: 'info',
-          title: 'Sesión Expirada',
-          text: 'Has sido desconectado por inactividad. Por favor, inicia sesión nuevamente.',
-          showConfirmButton: true
-        });
-      }
-    },
-    mounted() {
-      this.resetInactivityTimeout();
-      window.addEventListener('mousemove', this.resetInactivityTimeout);
-      window.addEventListener('keydown', this.resetInactivityTimeout);
-      console.log('Inactivity timeout initialized');
-    },
-    beforeDestroy() {
       clearTimeout(this.inactivityTimeout);
-      window.removeEventListener('mousemove', this.resetInactivityTimeout);
-      window.removeEventListener('keydown', this.resetInactivityTimeout);
+      this.inactivityTimeout = setTimeout(this.logoutUser, this.logoutTime);
+      console.log('Inactivity timeout reset');
+    },
+    logoutUser() {
+      localStorage.removeItem('token');
+      localStorage.removeItem('username');
+      this.$router.push('/login');
+      this.$swal.fire({
+        icon: 'info',
+        title: 'Sesión Expirada',
+        text: 'Has sido desconectado por inactividad. Por favor, inicia sesión nuevamente.',
+        showConfirmButton: true
+      });
     }
-  };
+  },
+  mounted() {
+    this.resetInactivityTimeout();
+    window.addEventListener('mousemove', this.resetInactivityTimeout);
+    window.addEventListener('keydown', this.resetInactivityTimeout);
+    console.log('Inactivity timeout initialized');
+  },
+  beforeDestroy() {
+    clearTimeout(this.inactivityTimeout);
+    window.removeEventListener('mousemove', this.resetInactivityTimeout);
+    window.removeEventListener('keydown', this.resetInactivityTimeout);
+  }
+};
