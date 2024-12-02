ea28da0fc634a50788c0d256344dfcd69b21cb15
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2024-12-01 22:51:10 -0500

    ultimoCCCommit

--- src/mixins/inactivityLogout.js
+++ src/mixins/inactivityLogout.js
@@ -2,7 +2,7 @@ export default {
   data() {
     return {
       inactivityTimeout: null,
-      logoutTime: 4 * 60 * 1000 // 8 segundos en milisegundos
+      logoutTime: 8 * 1000 // 8 segundos en milisegundos
     };
   },
   methods: {
