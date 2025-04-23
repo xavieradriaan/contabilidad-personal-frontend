3ee31e553e6e79258c7a5908e60a478c2dd51ddf
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2025-04-23 10:32:19 -0500

    cambios para que no sea como gelatina la app

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,21 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+
 :root {
   --welcome-bg: var(--primary-color);
   --welcome-text: var(--accent-color);
